76b6094d718ef4552073175f4395e26956f87cc5
Victor <aue228lublupacanov@gmail.com>
2024-11-11 17:46:38 +0100

    started refact the code

---
+++ controllers/blogs.js
@@ -0,0 +1,10 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -1,43 +1,52 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-app.use(cors())
-app.use(express.json())
+// const blogSchema = new mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number
+// })
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+// const Blog = mongoose.model('Blog', blogSchema)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+// const mongoUrl = process.env.MONGODB_URI
+// mongoose.connect(mongoUrl)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+// app.use(cors())
+// app.use(express.json())
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// app.get('/api/blogs', (request, response) => {
+//     Blog
+//         .find({})
+//         .then(blogs => {
+//             response.json(blogs)
+//         })
+// })
+
+// app.post('/api/blogs', (request, response) => {
+//     const blog = new Blog(request.body)
+
+//     blog
+//         .save()
+//         .then(result => {
+//             response.status(201).json(result)
+//         })
+// })
+
+// const PORT = process.env.PORT
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
